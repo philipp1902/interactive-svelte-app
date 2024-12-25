cd7202ee645bae6099408d53f71ca95ee2909161
philipp1902 <philipp.maginot@hfg-gmuend.de>
2024-12-25 22:34:33 +0100

    Initialisiere Svelte + Vite Projekt mit grundlegenden Konfigurationsdateien und erste Komponenten

---
+++ animal-app/src/components/History.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { lastAnimal, lastName } from "../store.js";
+    import { onDestroy } from "svelte";
+
+    let animal;
+    let name;
+
+    const unsubscribeAnimal = lastAnimal.subscribe((value) => {
+        // wenn der store-wert sich ändert, wird der neue Wert in die lokale Variable animal geschrieben
+        animal = value;
+    });
+
+    const unsubscribeName = lastName.subscribe((value) => {
+        name = value;
+    });
+
+    onDestroy(() => {
+        // Unsubscribe-Funktionen sind die Rückgabewerte einer Subscription
+        unsubscribeAnimal();
+        unsubscribeName();
+    });
+</script>
+
+<div class="history">
+    <h3>Last Entry</h3>
+    <p>Name: {name}</p>
+    <p>Animal: {animal}</p>
+</div>
+
+<style>
+    .history {
+        box-sizing: border-box;
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 1em;
+        background-color: #333;
+        color: white;
+        height: 100vh;
+    }
+</style>
